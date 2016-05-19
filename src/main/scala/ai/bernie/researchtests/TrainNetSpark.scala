845b5220f76d760af976409c0ac152752bac058e
crockpotveggies <crockpotveggies@users.noreply.github.com>
2016-05-18 19:24:32 -0700

    Update TrainNetSpark.scala

--- src/main/scala/ai/bernie/researchtests/TrainNetSpark.scala
+++ src/main/scala/ai/bernie/researchtests/TrainNetSpark.scala
@@ -1,6 +1,7 @@
 package ai.bernie.researchtests
 
 import java.util
+import java.util.Collections
 import java.util.concurrent.TimeUnit
 
 import org.apache.spark.{SparkConf, SparkContext}
@@ -13,24 +14,19 @@ import org.deeplearning4j.earlystopping.EarlyStoppingConfiguration
 import org.deeplearning4j.earlystopping.saver.InMemoryModelSaver
 import org.deeplearning4j.earlystopping.termination.{MaxEpochsTerminationCondition, MaxTimeIterationTerminationCondition}
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork
+import org.deeplearning4j.optimize.api.IterationListener
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener
 import org.deeplearning4j.spark.earlystopping.{SparkDataSetLossCalculator, SparkEarlyStoppingTrainer}
 import org.deeplearning4j.spark.impl.multilayer.SparkDl4jMultiLayer
-import org.deeplearning4j.ui.weights.HistogramIterationListener
+//import org.deeplearning4j.ui.weights.HistogramIterationListener
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4j.linalg.dataset.DataSet
 import org.slf4j.{Logger, LoggerFactory}
 
+//import scala.collection.JavaConversions._
 import scala.collection.mutable
 
 
-/**
-  * TrainNetSpark trains a convolutional neural network on Apache Spark using
-  * Deeplearning4j. An early stopping method is used to prevent overfitting.
-  *
-  * 9383 individual photos in original test
-  * Each image is a PNG 100 x 100 RGB
-  *
-  */
 object TrainNetSpark {
 
   lazy val log: Logger = LoggerFactory.getLogger(TrainNetSpark.getClass)
@@ -39,13 +35,28 @@ object TrainNetSpark {
 
     println(s"\n\n\nRuntime max memory ${Runtime.getRuntime().maxMemory()}\n\n\n")
 
+    println("Testing MKL lib functionality...")
+    import org.nd4j.linalg.api.ndarray.INDArray;
+    import org.nd4j.linalg.api.ops.impl.transforms.SoftMax;
+    import org.nd4j.linalg.api.shape.Shape;
+    import org.nd4j.linalg.factory.Nd4j;
+    import org.nd4j.linalg.factory.Nd4jBackend;
+    import org.nd4j.linalg.indexing.NDArrayIndex;
+    val input = Nd4j.create(Array[Double]( -0.75, 0.58, 0.42, 1.03, -0.61, 0.19, -0.37, -0.40, -1.42, -0.04)).transpose();
+    System.out.println("Input transpose " + Shape.shapeToString(input.shapeInfo()));
+    val output = Nd4j.create(10,1);
+    System.out.println("Element wise stride of output " + output.elementWiseStride());
+    Nd4j.getExecutioner().exec(new SoftMax(input, output));
+
     // create spark context
     val sparkConf = new SparkConf()
-    sparkConf.setMaster("local[2]")
-    sparkConf.setAppName("SIGFPEDOOMSDAY")
-    //sparkConf.set("spark.executor.memory","4g")
-    //sparkConf.set("spark.driver.memory","4g")
-    sparkConf.set(SparkDl4jMultiLayer.AVERAGE_EACH_ITERATION, String.valueOf(true))
+//    sparkConf.setMaster("local[2]")
+    sparkConf.setAppName("DEBUGAPP")
+//    sparkConf.set("spark.yarn.am.memory", "8G")
+//    sparkConf.set("spark.executor.memory","16G")
+//    sparkConf.set("spark.driver.memory","8G")
+//    sparkConf.set("spark.driver.cores", "4")
+    sparkConf.set(SparkDl4jMultiLayer.AVERAGE_EACH_ITERATION, String.valueOf(false))
     val sc = new JavaSparkContext(sparkConf)
 
     // neural network parameters
@@ -65,15 +76,14 @@ object TrainNetSpark {
 
     // load datasets
 
-    // training dataset (parallelized with Spark)
-    log.info("Load training data...")
-    val trainLabels = new java.util.ArrayList[String]()
-    val trainRecordReader = new ImageRecordReader(imageWidth, imageHeight, nChannels, true, trainLabels)
-    trainRecordReader.initialize(new FileSplit(new java.io.File("./cnn_dataset")))
-    //val dataSetIterator: DataSetIterator = new RecordReaderDataSetIterator(recordReader, batchSize, imageWidth*imageHeight*nChannels, labels.size())
-    val trainingSetIterator: DataSetIterator = new RecordReaderDataSetIterator(trainRecordReader, batchSize, -1, trainLabels.size())
+    // training dataset
+    println("Load training data...")
 
-    log.info(s"\n\nNumber of labels: ${trainLabels}\n\n")
+    val trainRecordReader = new ImageRecordReader(imageWidth, imageHeight, nChannels, true)
+    trainRecordReader.initialize(new FileSplit(new java.io.File("./cnn_dataset")))
+
+    //val dataSetIterator: DataSetIterator = new RecordReaderDataSetIterator(recordReader, batchSize, imageWidth*imageHeight*nChannels, labels.size())
+    val trainingSetIterator: DataSetIterator = new RecordReaderDataSetIterator(trainRecordReader, batchSize, -1, 8)
 
     // parallelize the dataset
     val list = new util.ArrayList[DataSet](numSamples)
@@ -83,23 +93,25 @@ object TrainNetSpark {
     // perform training
 
     // configure the early stopping trainer
-    //TODO val listener: LoggingEarlyStoppingListener = new LoggingEarlyStoppingListener()
+    val earlyStoppingListener: LoggingEarlyStoppingListener = new LoggingEarlyStoppingListener()
 
     val esConf = new EarlyStoppingConfiguration.Builder()
       .epochTerminationConditions(new MaxEpochsTerminationCondition(30))
-      .iterationTerminationConditions(new MaxTimeIterationTerminationCondition(20, TimeUnit.MINUTES))
+      .iterationTerminationConditions(new MaxTimeIterationTerminationCondition(12, TimeUnit.HOURS))
       .scoreCalculator(new SparkDataSetLossCalculator(trainRDD, true, sc))
       .evaluateEveryNEpochs(1)
       .modelSaver(new InMemoryModelSaver())
       .build()
 
     val nn = new MultiLayerNetwork(CNN.multiLayerConf(seed, iterations, nChannels, outputNum, imageWidth, imageHeight))
-    nn.setListeners(new HistogramIterationListener(1))
+//    nn.setListeners(new HistogramIterationListener(1))
+    nn.setListeners(Collections.singletonList(new ScoreIterationListener(listenerFreq).asInstanceOf[IterationListener]))
 
-    val trainer: SparkEarlyStoppingTrainer = new SparkEarlyStoppingTrainer(sc, esConf, nn, trainRDD, 50, 150, 5)
+    val trainer: SparkEarlyStoppingTrainer = new SparkEarlyStoppingTrainer(sc, esConf, nn, trainRDD, 100, numSamples, 20, earlyStoppingListener)
 
     val result = trainer.fit
     val bestModel: MultiLayerNetwork = result.getBestModel
+    println("Score of best model is: "+bestModel.score().toString())
 
     //TODO serialize bestModel and put it somewhere
   }
